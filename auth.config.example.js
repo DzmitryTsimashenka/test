e4bc936ad7fb8541f7957816160c7dc2e8a5c23a
DESKTOP-45ANRF3\Dzmitry <greentee@mail.ru>
2022-09-10 12:34:04 +0300

    - Add an auth config file.

---
+++ auth.config.example.js
@@ -0,0 +1,12 @@
+/**
+ * Credentials for local development.
+ * If you do a local development separately from main App
+ * you need to create an auth.config.js file and use your credentials.
+ *
+ * @type {{password: string, userName: string}}
+ */
+
+const auth = {
+    userName: 'JohnDove',
+    password: 'password',
+}
